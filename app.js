9cd053879bd486b2c8bd28e697de4929c5a1a200
David Maier <david.maier@couchbase.com>
2015-07-21 20:57:33 +0200

    Added first functionality

--- app.js
+++ app.js
@@ -3,27 +3,6 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var http = require('http');
 
-//Couchbase requirements and connection initialization
-var CB_HOST = '192.168.7.183';
-var CB_BUCKET = 'social';
-var CB_PWD = 'test';
-
-var couchbase = require('couchbase');
-
-var cluster = new couchbase.Cluster('couchbase://' + CB_HOST);
-var bucket = cluster.openBucket(CB_BUCKET, CB_PWD);
-var viewQuery = new couchbase.ViewQuery();
-    
-if (cluster && bucket && viewQuery)
-{
-    console.log("cluster = " + JSON.stringify(cluster));
-    console.log("bucket = " + JSON.stringify(bucket));
-}
-else
-{
-    console.log("ERROR: Could not initialize Couchbase");
-}
-
 //The express application initialization
 var app = express();
 
@@ -31,96 +10,23 @@ var app = express();
 app.use('/',express.static(__dirname + '/public'));
 app.use('/bower_components',express.static(__dirname + '/bower_components'));
 
-//The service implementation
+//The service base URL
 var SERVICE_URL = '/service/';
 
-/**
- * Helper to check if the variable is defined
- */
-function isDefined(obj)
-{  
-    if (typeof obj == 'undefined' || obj == 'undefined' || obj == null)
-    {
-        return false;
-    }
-    
-    return true;
-}
+//Couchbase
+var cb = require('./cb.js');
+var con = cb.connect();
 
-/**
- * Add a document
- */
-app.post(SERVICE_URL + 'add', function (req, res) {
-	
-    var id = req.query.id;
-    var msg = req.query.msg;
-        
-    
-    if (isDefined(id) && isDefined(msg))
-    {
-        var key = "msg::" + id;
-        var doc  = { 'msg' : msg };
+//Routes
+var sessions = require('./routes/Session.js');
+app.use(SERVICE_URL + "sessions/", sessions);
 
-        bucket.insert( key, doc, function(err, cbres) {
-         
-            if (err)
-            {
-                var emsg = "Could not add the document!";
-                console.log("ERROR" + emsg);
-                res.json({ "error" : emsg });
-                
-            }
-            else
-            {
-                console.log("Added " + key + " to Couchbase");
-                res.json({ 'success' : true });
-            }
-        });      
-    }
-    else
-    {
-        var emsg = "Did you pass all mandatory parameters?";
-        console.log("ERROR:" + emsg); 
-        res.json({"error" : emsg});
-    }    
-});
+var demos = require('./routes/Demo.js');
+app.use(SERVICE_URL, demos);
 
+var users = require('./routes/User.js');
+app.use(SERVICE_URL + "users/", users);
 
-/**
- * Get a document
- */
-app.get(SERVICE_URL + 'get', function (req, res) {
-	
-    var id = req.query.id;
-    
-    if (isDefined(id))
-    {
-        var key = "msg::" + id;
-
-        bucket.get(key, function(err, cbres) {
-          
-            if (err)
-            {
-                var emsg = "Could not get the document!";
-                console.log("ERROR" + emsg);
-                res.json({ "error" : emsg });
-                
-            }
-            else
-            {
-                console.log("Got " + JSON.stringify(cbres));
-                res.json(cbres);
-            }
-            
-        }); 
-    }
-    else
-    {
-        var emsg = "Did you pass all mandatory parameters?";
-        console.log("ERROR:" + emsg); 
-        res.json({"error" : emsg});
-    }    
-});
 
 //Web server
 server = app.listen(9000, function () { 
